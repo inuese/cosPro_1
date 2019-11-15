457c11e2e195948de1d4f48377d99b46cdbd9f00
evitcepsrep <kv145826@gmail.com>
2019-11-15 16:36:52 +0900

    정답 / 코드를 더 줄여볼 수 있었지만 함수를 따로 구분하는게 더 좋아보임

---
+++ py1-2-6/tmp.py
@@ -0,0 +1,27 @@
+#다음과 같이 import를 사용할 수 있습니다.
+#import math
+def move(str):
+    if str=='U':
+        return [0,1]
+    elif str == 'D':
+        return [0,-1]
+    elif str == 'L':
+        return [-1,0]
+    elif str == 'R':
+        return [1,0]
+
+def solution(commands):
+    # 여기에 코드를 작성해주세요.
+    answer = [0,0]
+    for i in range(len(commands)):
+        pos = move(commands[i])
+        answer[0] += pos[0]
+        answer[1] += pos[1]
+    return answer
+
+#아래는 테스트케이스 출력을 해보기 위한 코드입니다.
+commands = "URDDL"
+ret = solution(commands)
+
+#[실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+print("solution 함수의 반환 값은 ", ret, " 입니다.")
